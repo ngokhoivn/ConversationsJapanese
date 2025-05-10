957c72581090d5d88076b4dc1ccd6bcb5cc0c808
ngokhoivn <52380531+ngokhoivn@users.noreply.github.com>
2025-05-10 14:00:28 +0700

    Update ChatWithAI.js

--- ChatWithAI.js
+++ ChatWithAI.js
@@ -246,7 +246,7 @@ ${recentMessages}
         };
 
         const copyBtn = document.createElement('button');
-        copyBtn.class | `copy-btn` = document.createElement('button');
+        
         copyBtn.className = 'icon-btn';
         copyBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 24 24">
             <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"/>
